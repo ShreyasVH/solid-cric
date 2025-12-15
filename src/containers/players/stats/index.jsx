0ff9b3cf770d0c46245634279a4c2f9077756d10
ShreyasVH <shreyas.deve.1@gmail.com>
2025-12-15 23:35:34 +0530

    removed position filter

--- src/containers/players/stats/index.jsx
+++ src/containers/players/stats/index.jsx
@@ -206,12 +206,7 @@ export default function PlayerStats() {
         year: {
             displayName: 'Year',
             type: FILTER_TYPE.RANGE
-        },
-        number: {
-            displayName: 'Position',
-            type: FILTER_TYPE.CHECKBOX,
-            values: [...Array(11).keys()].map(i => ({ id: String(i + 1), name: i + 1 }))
-        },
+        }
     });
 
     onMount(async () => {
