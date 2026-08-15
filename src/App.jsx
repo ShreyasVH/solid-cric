5f9d70f2ac544f5935a9350fad8374d6a6f9a59f
ShreyasVH <shreyas.deve.1@gmail.com>
2026-08-15 23:52:13 +0530

    support dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Loader from './components/loader';
 import { Router, Route } from "@solidjs/router";
 import routes from './routes';
 import rootLayout from './rootLayout.jsx';
+import { useTheme } from "@suid/material/styles";
 
 function App() {
+    const theme = useTheme();
+
     return (
-    <>
+    <div data-theme={theme.palette.mode}>
         <Router root={rootLayout}>
             <For each={routes}>{route =>
                 <Route path={route.path} component={route.component} />
             }</For>
         </Router>
-    </>
+    </div>
   )
 }
 
