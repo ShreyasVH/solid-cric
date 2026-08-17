ce732ea436ca3c4f4b1311016f9948f6272648d2
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2026-08-17 12:17:02 +0530

    support for searching players
    * intermediate commit
    
    * hiding dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import AppBar from './components/appBar'
 import Loader from './components/loader';
 import { Router, Route } from "@solidjs/router";
 import routes from './routes';
+import rootLayout from './rootLayout.jsx';
 
 function App() {
     return (
     <>
-        <AppBar />
-        <Loader />
-        <div style={{padding: '2%'}}>
-            <Router>
-                <For each={routes}>{route =>
-                    <Route path={route.path} component={route.component} />
-                }</For>
-            </Router>
-        </div>
+        <Router root={rootLayout}>
+            <For each={routes}>{route =>
+                <Route path={route.path} component={route.component} />
+            }</For>
+        </Router>
     </>
   )
 }
--- src/components/appBar/index.jsx
+++ src/components/appBar/index.jsx
@@ -7,8 +7,17 @@ import {
     Toolbar,
     Typography,
 } from "@suid/material";
+import { useNavigate } from '@solidjs/router';
+import SearchSelect from '../searchSelect';
 
 function AppBarComponent() {
+    const navigate = useNavigate();
+
+    const handlePlayerSelect = (event, item) => {
+        const url = `/players/details?id=${item.id}`;
+        navigate(url);
+    };
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -23,6 +32,45 @@ function AppBarComponent() {
                         >
                             <MenuIcon />
                         </IconButton>
+
+                        <Box
+                            sx={{
+                                ml: "auto",
+
+                                "& label.MuiInputLabel-root": {
+                                    color: "rgba(255, 255, 255, 0.38)"
+                                },
+                                "& .Mui-focused": {
+                                    "& fieldset.MuiOutlinedInput-notchedOutline": {
+                                        borderColor: "rgba(255, 255, 255, 0.38) !important",
+                                    },
+                                    "& input::placeholder": {
+                                        color: "red !important",
+                                        opacity: 1
+                                    },
+                                    color: "red",
+                                    "& input": {
+                                        color: "rgba(255, 255, 255, 0.38)"
+                                    }
+                                },
+                                "& label.Mui-focused": {
+                                    color: "rgba(255, 255, 255, 0.38)"
+                                },
+                                "& .MuiOutlinedInput-root": {
+                                    "& .MuiOutlinedInput-input": {
+                                        color: "rgba(255, 255, 255, 0.38)"
+                                    },
+                                    "& fieldset.MuiOutlinedInput-notchedOutline": {
+                                        borderColor: "rgba(255, 255, 255, 0.38)",
+                                    },
+                                    "&:hover fieldset.MuiOutlinedInput-notchedOutline": {
+                                        borderColor: "rgba(255, 255, 255, 0.38)"
+                                    }
+                                }
+                            }}
+                        >
+                            <SearchSelect onSelect={handlePlayerSelect} />
+                        </Box>
                     </Toolbar>
                 </AppBar>
             </Box>
